33a37a1b2e444e7898889cda82ad3d6b1e132d20
m4rquee <lucasxm21@hotmail.com>
2018-04-17 07:54:31 -0300

    Theoretically working

--- lab08.py
+++ lab08.py
@@ -2,11 +2,14 @@ from math import ceil
 
 n = int(input())
 db = [input().split() for i in range(n)]
-db = [v for v in map(lambda e: (e[0], round(int(e[2]) / int(e[1]), 4)), db)]
+
+avg_map = lambda e: (e[0], float(e[2]) / float(e[1]))
+db = [v for v in map(avg_map, db)]
+
 esp = {e[0]: 0 for e in db}
 for k in esp.keys():
     mults = [v[1] for v in filter(lambda e: e[0] == k, db)]
-    esp[k] = round(sum(mults) / len(mults), 4)
+    esp[k] = sum(mults) / len(mults)
 
 
 def read_line():
@@ -16,5 +19,5 @@ def read_line():
         line = input()
 
 
-result = [ceil(esp[q[0]] * int(q[1]), 1) for q in read_line()]
+result = [ceil(esp[q[0]] * float(q[1])) for q in read_line()]
 print(*result, sep='\n')
