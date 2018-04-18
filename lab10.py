b988654ef1104385d5ccecfe9053633815393b4c
m4rquee <lucasxm21@hotmail.com>
2018-04-18 19:40:52 -0300

    Almost working

--- lab10.py
+++ lab10.py
@@ -2,6 +2,7 @@
 import re
 
 
+# Recebe as operações
 def read_ops():
     line = input()
     while line.upper() != 'Q':
@@ -9,7 +10,7 @@ def read_ops():
         line = input()
 
 
-reg_pal = '((?<=^)|(?<=\s))%s(?!\w)'  # Regex para dar match em palavras
+reg_pal = '(?<!\w)%s(?!\w)'  # Regex para dar match em palavras separadas
 ops_nargs = {'D': 1 + 1, 'I': 1 + 1, 'R': 2 + 1}  # Número de parâmetros de cada operação, por conveniência soma-se 1
 
 texto = input()
@@ -19,21 +20,28 @@ i = 0
 while i < len(cmds):
     print(texto)
     operacao = cmds[i].upper()
-    nargs = ops_nargs[operacao]
+    nargs = ops_nargs[operacao]  # Número de parâmetros dessa operação
+
     args = cmds[i + 1:i + nargs]  # Parâmetros da operação
     i += nargs  # Pula para a próxima operação
 
-    new_str = ''  # String a ser colocada no lugar do argumento da operação
     if operacao == 'I':
         reg = re.compile(reg_pal % args[0], flags=re.IGNORECASE)
-        new_str = reg.search(texto).group()[::-1]
-    elif operacao == 'R':
-        new_str = args[1]
+        matches = reg.findall(texto)
+        for match in matches:
+            texto = re.sub(match, match[::-1], texto, 1)
+    else:
+        if operacao == 'R':
+            new_str = args[1]
+        elif operacao == 'D':
+            new_str = ''
 
-    # Substitui o argumento da operação pela nova str:
-    texto = re.sub(reg_pal % args[0], new_str, texto, flags=re.IGNORECASE)
+        # Substitui o argumento da operação pela nova string:
+        texto = re.sub(reg_pal % args[0], new_str, texto, flags=re.IGNORECASE)
 
-    texto = re.sub('\s+', ' ', texto)
-    texto = re.sub('\s\W', '', texto)
+        if operacao == 'D':
+            texto = re.sub('\s[,\.\?\!\:]', '', texto)
+            texto = re.sub('\s\s+', ' ', texto)
 
+texto = re.sub('\s\s+', ' ', texto)
 print(texto)
