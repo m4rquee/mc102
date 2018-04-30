c8c5bf78493fb61e9db6ade3eec36e381e838f2c
m4rquee <lucas.silva@dextra-sw.com>
2018-04-30 12:18:58 -0300

    lab13 structure

---
+++ lab13/lab13.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# *******************************************************************************
+# Funcao: atualizaTabela
+#
+# Parametros:
+#   tabela: uma matriz com os dados da tabela do campeonato
+#   jogo: string contendo as informações de um jogo no formato especificado no lab.
+#
+# Descrição:
+#   Deve inserir as informações do parametro 'jogo' na tabela.
+#   OBSERVAÇÃO: nesse momento não é necessário ordenar a tabela, apenas inserir as informações.
+def atualizaTabela(tabela, jogo):
+    pass
+
+
+#  -- INSIRA SEU CÓDIGO AQUI -- #
+# *******************************************************************************
+
+# *******************************************************************************
+# Funcao: comparaTimes
+#
+# Parametros:
+#   time1: informações de um time
+#   time2: informações de um time
+#
+# Descricão:
+#   retorna 1, se o time1>time2, retorna -1, se time1<time2, e retorna 0, se time1=time2
+#   Observe que time1>time2=true significa que o time1 deve estar em uma posição melhor do que o time2 na tabela.
+def comparaTimes(time1, time2):
+    pass
+
+
+#  -- INSIRA SEU CÓDIGO AQUI -- #
+# *******************************************************************************
+
+
+# *******************************************************************************
+# Funcao: ordenaTabela
+#
+# Parametros:
+#   tabela: uma matriz com os dados da tabela do campeonato.
+#
+# Descricão:
+#   Deve ordenar a tabela com campeonato de acordo com as especificaçoes do lab.
+#
+def ordenaTabela(tabela):
+    pass
+
+
+#  -- INSIRA SEU CÓDIGO AQUI -- #
+# *******************************************************************************
+
+
+# *******************************************************************************
+# Funcao: imprimeTabela
+#
+# Parametros:
+#   tabela: uma matriz com os dados da tabela do campeonato.
+#
+# Descrição:
+#   Deve imprimir a tabela do campeonato de acordo com as especificações do lab.
+def imprimeTabela(tabela):
+    pass
+#  -- INSIRA SEU CÓDIGO AQUI -- #
+# *******************************************************************************
