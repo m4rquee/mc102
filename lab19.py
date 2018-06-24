49e36524b8748f81f73f08c17d2d5dc52cfba3e3
m4rquee <lucas.silva@dextra-sw.com>
2018-06-24 18:25:03 -0300

    lab19-Better version

--- lab19.py
+++ lab19.py
@@ -1,22 +1,16 @@
-n, k = [int(v) for v in input().split()]
-tree = [[int(v) for v in input().split()] for _ in range(n)]
+n, k = map(int, input().split())
+tree = [input().split() for _ in range(n)]
 
 
-def make_chain(chain, pos, found):
-    if found:
-        chain.add(pos)
-
-    found = found or pos == k
-
-    for j, c in enumerate(tree[pos]):
-        if c == 1:
-            chain |= make_chain(chain, j, found)
-
-    return chain
+def make_chain(c, pos):
+    if pos != k:
+        c.add(pos)
+    for j, e in enumerate(tree[pos]):
+        if e == '1':
+            c |= make_chain(c, j)
+    return c
 
 
-chain = make_chain(set([]), 0, False)
-chain = list(chain)
+chain = list(make_chain(set([]), k))
 chain.sort()
-chain = [str(k)] + list(map(str, chain))
-print(' '.join(chain))
+print(' '.join(map(str, [k] + chain)))
